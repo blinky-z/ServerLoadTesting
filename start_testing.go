cd6b444f259810851f806b538f53c77e046b8279
TwITe <dm.blink19@gmail.com>
2018-06-30 18:50:52 +0300

    big code update
    1. Added functions that handles request sending
    2. Added main function that creates anonymous or particular client
    2. Added messages logging

--- start_testing.go
+++ start_testing.go
@@ -3,180 +3,175 @@ package main
 import (
 	"sync"
 	"net/http"
-	"fmt"
 	"strconv"
 	"encoding/json"
 	"errors"
 	"io/ioutil"
 	"time"
+	"bytes"
+	"log"
+	"io"
+	"os"
 )
+
+var (
+	logInfo  *log.Logger
+	logError *log.Logger
+)
+
+func InitLoggers(infoHandle io.Writer, errorHandle io.Writer) {
+	logInfo = log.New(infoHandle, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+	logInfo.Output(0, "adasd")
+
+	logError = log.New(errorHandle, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+}
 
 const (
-	messagesNum = 10
 	warmUpClientsNum = 5
-	testClientsNum = 10
+	testClientsNum   = 10
+	testMessagesNum  = 10
 )
 
-type good struct {
+type item struct {
 	Name  string `json:"name"`
 	Price string `json:"price"`
 }
 
-type anonymousClientResponseBody struct {
-	Items []good `json:"items"`
-}
-
-type certainClientResponseBody struct {
-	Nickname string `json:"nickname"`
-	Items    []good `json:"items"`
-}
-
-type ResponseBody struct {
-	Nickname string `json:"nickname"`
-	Items    []good `json:"items"`
-}
-
-func getExpectedBodyAnonymousClient() string {
-	var defaultGoodsNumber int = 5
-	var multiplier int = 30
-
-	responseBody := &anonymousClientResponseBody{}
-
-	var goods []good = make([]good, defaultGoodsNumber)
-
-	for currentGoodNumber := 0; currentGoodNumber < defaultGoodsNumber; currentGoodNumber++ {
-		newGood := good{}
-		newGood.Name = "default" + strconv.Itoa(currentGoodNumber)
-		newGood.Price = strconv.Itoa(currentGoodNumber * multiplier)
-
-		goods[currentGoodNumber] = newGood
-	}
-
-	responseBody.Items = goods
-
-	jsonBody, _ := json.Marshal(responseBody)
-
-	response := string(jsonBody)
-
-	return response
-}
-
 func getExpectedResponseBody(userName string) string {
 	const defaultGoodsNumber int = 5
-	var multiplier int
 
-	responseBody := &ResponseBody{}
+	responseBody := map[string]interface{}{}
 
-	if userName != "" {
-		goods := make([]good, len(userName))
+	if userName != "" { // in case of username was passed
+		var multiplier int = 0
 
-		responseBody.Nickname = userName
+		items := make([]item, len(userName))
+
+		responseBody["nickname"] = userName
 		for _, charValue := range userName {
 			multiplier += int(charValue)
 		}
 
-		for currentGoodNumber := 0; currentGoodNumber < len(goods); currentGoodNumber++ {
-			newGood := good{}
-			newGood.Name = userName + strconv.Itoa(currentGoodNumber)
-			newGood.Price = strconv.Itoa((currentGoodNumber + 1) * multiplier)
+		for currentItemNumber := 0; currentItemNumber < len(items); currentItemNumber++ {
+			newItem := item{}
+			newItem.Name = userName + strconv.Itoa(currentItemNumber)
+			newItem.Price = strconv.Itoa((currentItemNumber + 1) * multiplier)
 
-			goods[currentGoodNumber] = newGood
+			items[currentItemNumber] = newItem
 		}
 
-		responseBody.Items = goods
+		responseBody["items"] = items
 
-	} else {
-		goods := make([]good, defaultGoodsNumber)
+	} else { // default case (anonymous client)
+		var multiplier int = 30
 
-		for currentGoodNumber := 0; currentGoodNumber < len(goods); currentGoodNumber++ {
-			newGood := good{}
-			newGood.Name = "default" + strconv.Itoa(currentGoodNumber)
-			newGood.Price = strconv.Itoa(currentGoodNumber * multiplier)
+		items := make([]item, defaultGoodsNumber)
 
-			goods[currentGoodNumber] = newGood
+		for currentItemNumber := 0; currentItemNumber < len(items); currentItemNumber++ {
+			newItem := item{}
+			newItem.Name = "default" + strconv.Itoa(currentItemNumber)
+			newItem.Price = strconv.Itoa(currentItemNumber * multiplier)
+
+			items[currentItemNumber] = newItem
 		}
 
-		responseBody.Items = goods
+		responseBody["items"] = items
 	}
 
 	jsonBody, _ := json.Marshal(responseBody)
-
-	response := string(jsonBody)
-
-	return response
+	return string(jsonBody)
 }
 
-func checkClientResponse(response *http.Response) error {
-	expectedBody := getExpectedBodyAnonymousClient()
+func checkClientResponse(userName string, response *http.Response) error {
+	expectedBody := getExpectedResponseBody(userName)
 
-	receivedResponseByteBody, err := ioutil.ReadAll(response.Body)
+	defer response.Body.Close()
+	receivedResponseBody, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		panic(err)
 	}
-	var convertedReceivedResponseBody = string(receivedResponseByteBody)
 
 	if response.StatusCode != 200 {
 		return errors.New("wrong status code")
 	}
 
-	if convertedReceivedResponseBody != expectedBody {
+	if string(receivedResponseBody) != expectedBody {
 		return errors.New("wrong response body")
 	}
 
 	return nil
 }
 
-func startAnonymousTestClient(currentClientNumber int, wg *sync.WaitGroup) {
-	defer wg.Done()
+func sendRequest(path, queryParams, body string) (*http.Response, error) {
+	url := "http://localhost:8080" + path
 
-	for currentMessageNumber := 0; currentMessageNumber < messagesNum; currentMessageNumber++ {
-		response, err := http.Get("http://localhost:8080/")
-		if err != nil {
-			panic(err)
+	var response *http.Response
+	var err error
+
+	if body == "" {
+		if queryParams != "" {
+			url += "?" + queryParams
 		}
 
-		fmt.Printf("[Goroutine %d] Message %d was successfully sent", currentClientNumber, currentMessageNumber)
-
-		if resultCheck := checkClientResponse(response); resultCheck != nil {
-			fmt.Printf("[Goroutine %d][Message %d] Got invalid response. Error Message: %s\n", currentMessageNumber, currentClientNumber, resultCheck)
-		} else {
-			fmt.Printf("[Goroutine %d][Message %d] Got valid response\n", currentMessageNumber, currentClientNumber)
-		}
+		response, err = http.Get(url)
+	} else {
+		response, err = http.Post(url, "application/json", bytes.NewBuffer([]byte(body)))
 	}
+
+	return response, err
 }
 
-func startWarmUpClient(currentClientNumber int, wg *sync.WaitGroup) {
+func startTestClient(path, queryParam, body string, currentClientNumber int, wg *sync.WaitGroup) {
 	defer wg.Done()
 
-	for currentMessageNumber := 0; currentMessageNumber < messagesNum; currentMessageNumber++ {
-		_, err := http.Get("http://localhost:8080/")
-
+	for currentMessageNumber := 0; currentMessageNumber < testMessagesNum; currentMessageNumber++ {
+		response, err := sendRequest(path, queryParam, body)
 		if err != nil {
 			panic(err)
 		}
 
-		fmt.Println("Message", currentMessageNumber, "was sent from goroutine", currentClientNumber)
+		logInfo.Printf("[Goroutine %d] Message %d was successfully sent\n", currentClientNumber, currentMessageNumber)
+
+		if resultCheck := checkClientResponse("", response); resultCheck != nil {
+			logError.Printf("[Goroutine %d][Message %d] Got invalid response. Error Message: %s\n", currentMessageNumber, currentClientNumber, resultCheck)
+		} else {
+			logInfo.Printf("[Goroutine %d][Message %d] Got valid response\n", currentMessageNumber, currentClientNumber)
+		}
 	}
 }
 
 func main() {
-	wg := &sync.WaitGroup{}
+	InitLoggers(os.Stdout, os.Stderr)
+
+	wgWarmUp := &sync.WaitGroup{}
 
 	// create clients to warm up a test ground
 	for currentClientNumber := 0; currentClientNumber < warmUpClientsNum; currentClientNumber++ {
-		wg.Add(1)
-		go startWarmUpClient(currentClientNumber, wg)
+		wgWarmUp.Add(1)
+
+		path, queryParams, requestBody := "/", "", ""
+
+		go startTestClient(path, queryParams, requestBody, currentClientNumber, wgWarmUp)
 	}
 	time.Sleep(time.Millisecond)
+
+	wgWarmUp.Wait()
+
+	logInfo.Println("[MAIN] Warm up is done")
+
+	wgTest := &sync.WaitGroup{}
 
 	// create clients for web server load testing
 	for currentClientNumber := 0; currentClientNumber < testClientsNum; currentClientNumber++ {
-		wg.Add(1)
-		go startAnonymousTestClient(currentClientNumber, wg)
+		wgTest.Add(1)
+
+		path, queryParams, requestBody := "/", "", ""
+
+		go startTestClient(path, queryParams, requestBody, currentClientNumber, wgTest)
 	}
 	time.Sleep(time.Millisecond)
 
-	wg.Wait()
+	wgTest.Wait()
 
-	fmt.Println("[MAIN] All tests are passed")
+	logInfo.Println("[MAIN] All tests are passed")
 }
